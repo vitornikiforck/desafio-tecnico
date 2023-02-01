303d83bc6295b80833af8459a2ab14fada1c13ca
Vitor Nikiforck Pereira <vitor.pereira@ndd.com.br>
2023-02-01 08:57:29 -0300

    added challenge resolution, question 1

---
+++ Questao1/Constantes.cs
@@ -0,0 +1,7 @@
+﻿namespace Questao1
+{
+    public static class Constantes
+    {
+        public const double TaxaSaque = 3.50;
+    }
+}
---
+++ Questao1/ContaBancaria.cs
@@ -0,0 +1,31 @@
+﻿namespace Questao1
+{
+    public class ContaBancaria
+    {
+        public int Numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; private set; }
+
+        public ContaBancaria(int numero, string titular, double depositoInicial = 0)
+        {
+            this.Numero = numero;
+            this.Titular = titular;
+            this.Saldo = depositoInicial;
+        }
+
+        internal void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        internal void Saque(double quantia)
+        {
+            Saldo -= quantia + Constantes.TaxaSaque;
+        }
+
+        public override string ToString()
+        {
+            return $"Conta {this.Numero}, Títular: {this.Titular}, Saldo: $ {this.Saldo.ToString("F")}";
+        }
+    }
+}
